5b2a9ae21830d33c38af3c27f2ea6f68ff7e7ec2
MOHAMMED UMAR FAAZITH K <123172380+MOHAMMEDUMARFAAZITH@users.noreply.github.com>
2023-06-11 21:56:59 +0530

    Update BMS.py

--- BMS.py
+++ BMS.py
@@ -1,7 +1,7 @@
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
-Sales_model = pickle.load(open("E:\GUVI\ML_project\Big mart sales\Big_mart_sales_prediction.sav","rb"))
+Sales_model = pickle.load(open("Big_mart_sales_prediction.sav","rb"))
 with st.sidebar:
 	
 	selected = option_menu("sales_prediction",["Big_mart_sales_prediction"],default_index=0)
